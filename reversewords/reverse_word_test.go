ac168718f6160be63bce1a01dfd7c45db7f2b2b1
Eduardo Antonio Campos Martinez <eduardo.camposmartinez@lifemiles.com>
2024-10-14 16:57:52 -0600

    Change test to reverse words challenge

--- reversewords/reverse_word_test.go
+++ reversewords/reverse_word_test.go
@@ -9,9 +9,9 @@ func TestReverseWord(t *testing.T) {
 		expected string
 	}{
 		{"Test 1", "Hello World", "olleH dlroW"},
-		{"Test 2", "Hello  World", "olleH  dlroW"},
-		{"Test 3", "Hello World ", "olleH dlroW "},
-		{"Test 4", " Hello World", " olleH dlroW"},
+		{"Test 2", "This is an example!", "sihT si na !elpmaxe"},
+		{"Test 3", "double  spaces", "elbuod  secaps"},
+		{"Test 4", "This is a test", "sihT si a tset"},
 	}
 	t.Run("Test Reverse Words", func(t *testing.T) {
 		for _, tt := range tests {
